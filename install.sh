17eaaae4d35d7ae1871dd8c29beb55e672057907
sletteon <misi.g126@gmail.com>
2017-09-29 19:28:19 +0200

    Hibajavítás

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 sudo apt update
 sudo apt install libqt5webkit5-dev
 sudo rm -r /usr/bin/mbrowser
-sudo cp ~/home/bongeszo/build/mbrowser /usr/bin
+sudo cp ~/bongeszo/build/mbrowser /usr/bin
 sudo chmod +x /usr/bin/mbrowser
 
--- sajatinstall.sh
+++ sajatinstall.sh
@@ -5,6 +5,6 @@
 sudo apt update
 sudo apt install libqt5webkit5-dev
 sudo rm -r /usr/bin/mbrowser
-sudo cp ~/home/Projects/bongeszo/build/mbrowser /usr/bin
+sudo cp ~/Projects/bongeszo/build/mbrowser /usr/bin
 sudo chmod +x /usr/bin/mbrowser
 

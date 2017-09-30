e4687b6ce9cb4283438ebc12ae03d41f0d0c0ecd
sletteon <Sletteon@users.noreply.github.com>
2017-09-30 08:41:39 +0200

    Update sajatinstall.sh

--- sajatinstall.sh
+++ sajatinstall.sh
@@ -2,8 +2,6 @@
 # Saját script
 # Frissítésekhez készítettem magamnak, hogy ne kelljen mindig a Projects mappát a cp parancsba beilleszteni
 # Valószínüleg csak nálam működik, mivel nekem van Projects mappámban a bongeszo mappa
-sudo apt update
-sudo apt install libqt5webkit5-dev
 sudo rm -r /usr/bin/mbrowser
 sudo cp ~/Projects/bongeszo/build/mbrowser /usr/bin
 sudo chmod +x /usr/bin/mbrowser
